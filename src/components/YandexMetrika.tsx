01fab6a15b95ce92e5b03953a48b20a751f0cda7
Pavel <klon_008@mail.ru>
2025-08-13 04:25:45 +0700

    metrika

--- src/components/YandexMetrika.tsx
+++ src/components/YandexMetrika.tsx
@@ -11,26 +11,21 @@ export default function YandexMetrika() {
         <>
             <Script id="yandex-metrika" strategy="afterInteractive">
                 {`
-          (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0];
-              k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
-          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${ymId}', 'ym');
 
-          ym(${ymId}, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-          });
+    ym(${ymId}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
         `}
             </Script>
             <noscript>
                 <div>
                     <img
                         src={`https://mc.yandex.ru/watch/${ymId}`}
-                        style={{ position: 'absolute', left: '-9999px' }}
+                        style={{position: 'absolute', left: '-9999px'}}
                         alt=""
                     />
                 </div>
